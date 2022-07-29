d12f9428192c6f510f10024a34a04fe71ad2c19b
Rishant Kumar <kumarrkrishant@gmail.com>
2022-07-29 23:24:16 +0530

    Updates

---
+++ precache-manifest.9b6515b2a2af7aaf3dea2a9f5f3c7d10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4aae860d4bc271f194",
+    "url": "/Chat-App/static/css/main.fcbe536c.chunk.css"
+  },
+  {
+    "revision": "da4aae860d4bc271f194",
+    "url": "/Chat-App/static/js/main.da4aae86.chunk.js"
+  },
+  {
+    "revision": "872924df89951a09ee3a",
+    "url": "/Chat-App/static/js/1.872924df.chunk.js"
+  },
+  {
+    "revision": "16e5dd3ee04c4f557f09",
+    "url": "/Chat-App/static/js/runtime~main.16e5dd3e.js"
+  },
+  {
+    "revision": "97ce0e19992ccebda16ddf330dc79447",
+    "url": "/Chat-App/index.html"
+  }
+];
